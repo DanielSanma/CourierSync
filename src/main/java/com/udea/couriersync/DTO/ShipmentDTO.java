5ff037b2894f872789cceeedcb65ab3142997387
DanielSanma <desteban.sanchez@udea.edu.co>
2025-11-09 19:32:06 -0500

    Builder

--- src/main/java/com/udea/couriersync/DTO/ShipmentDTO.java
+++ src/main/java/com/udea/couriersync/DTO/ShipmentDTO.java
@@ -2,7 +2,15 @@ package com.udea.couriersync.DTO;
 
 import com.udea.couriersync.enums.ShipmentPriority;
 import com.udea.couriersync.enums.ShipmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShipmentDTO {
     private Long id;
     private String trackingCode;
@@ -14,102 +22,4 @@ public class ShipmentDTO {
     private Double volume;
     private ShipmentPriority priority;
     private ShipmentStatus status;
-
-    public ShipmentDTO() {
-    }
-
-    public ShipmentDTO(Long id, String trackingCode, ClientDTO client, VehicleDTO vehicle,
-            String originAddress, String destinationAddress, Double weight, Double volume,
-            ShipmentPriority priority, ShipmentStatus status) {
-        this.id = id;
-        this.trackingCode = trackingCode;
-        this.client = client;
-        this.vehicle = vehicle;
-        this.originAddress = originAddress;
-        this.destinationAddress = destinationAddress;
-        this.weight = weight;
-        this.volume = volume;
-        this.priority = priority;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTrackingCode() {
-        return trackingCode;
-    }
-
-    public void setTrackingCode(String trackingCode) {
-        this.trackingCode = trackingCode;
-    }
-
-    public ClientDTO getClient() {
-        return client;
-    }
-
-    public void setClient(ClientDTO client) {
-        this.client = client;
-    }
-
-    public VehicleDTO getVehicle() {
-        return vehicle;
-    }
-
-    public void setVehicle(VehicleDTO vehicle) {
-        this.vehicle = vehicle;
-    }
-
-    public String getOriginAddress() {
-        return originAddress;
-    }
-
-    public void setOriginAddress(String originAddress) {
-        this.originAddress = originAddress;
-    }
-
-    public String getDestinationAddress() {
-        return destinationAddress;
-    }
-
-    public void setDestinationAddress(String destinationAddress) {
-        this.destinationAddress = destinationAddress;
-    }
-
-    public Double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Double weight) {
-        this.weight = weight;
-    }
-
-    public Double getVolume() {
-        return volume;
-    }
-
-    public void setVolume(Double volume) {
-        this.volume = volume;
-    }
-
-    public ShipmentPriority getPriority() {
-        return priority;
-    }
-
-    public void setPriority(ShipmentPriority priority) {
-        this.priority = priority;
-    }
-
-    public ShipmentStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ShipmentStatus status) {
-        this.status = status;
-    }
 }
